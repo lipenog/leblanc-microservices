c65b5ec0eb0ced9cbb55239780466c9f6c2faf49
lipenog <luizfenogueira@outlook.com>
2024-12-06 17:44:33 -0300

    added users service

---
+++ users-service/src/main/java/com/example/usersservice/web/service/UsersService.java
@@ -0,0 +1,16 @@
+package com.example.usersservice.web.service;
+
+import com.example.usersservice.web.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsersService {
+    private final UsersRepository usersRepository;
+
+    @Autowired
+    public UsersService(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+    
+}
