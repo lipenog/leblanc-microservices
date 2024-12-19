094e2a20d58731ba1074a5879d310522e602dc2f
lipenog <luizfenogueira@outlook.com>
2024-12-19 13:42:25 -0300

    added post search query

--- elastic-search-service/src/main/java/com/example/elasticsearchservice/web/repository/ElasticRepository.java
+++ elastic-search-service/src/main/java/com/example/elasticsearchservice/web/repository/ElasticRepository.java
@@ -2,7 +2,42 @@ package com.example.elasticsearchservice.web.repository;
 
 
 import com.example.elasticsearchservice.web.entity.Posts;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
+import org.springframework.stereotype.Repository;
+import java.util.List;
+@Repository
 public interface ElasticRepository extends ElasticsearchRepository<Posts, String> {
+    @Query("""
+        "bool": {
+        "must":[
+          {
+            "match": {
+              "content": "#{#content}"
+            }
+          },
+          {
+            "match": {
+                "media_content": "#{#content}"
+            }
+          }
+        ],
+        "should": [
+          {
+            "match_phrase": {
+              "content": "#{#tags}"
+            }
+          }
+        ]
+        }
+        },
+        "highlight": {
+        "fragment_size": 500,
+        "fields": {
+            "content": {
+            }
+        }
+        }
+    """)
+    List<Posts> getPostsBySearch(String content, String tags);
 }
