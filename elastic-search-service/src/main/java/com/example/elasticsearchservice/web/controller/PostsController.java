ea23dcbcf465657327b7104ea46158ba474298e1
lipenog <luizfenogueira@outlook.com>
2025-01-06 18:32:41 -0300

    optimized imports

--- elastic-search-service/src/main/java/com/example/elasticsearchservice/web/controller/PostsController.java
+++ elastic-search-service/src/main/java/com/example/elasticsearchservice/web/controller/PostsController.java
@@ -1,10 +1,8 @@
 package com.example.elasticsearchservice.web.controller;
 
 import com.example.elasticsearchservice.web.dto.PostsSearchDTO;
-import com.example.elasticsearchservice.web.entity.Posts;
 import com.example.elasticsearchservice.web.service.PostsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.SearchHits;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
