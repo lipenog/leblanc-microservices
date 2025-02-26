9b1ddde8e506b5841c22885fcec32f84a7480b8e
lipenog <luizfenogueira@outlook.com>
2025-02-26 14:48:56 -0300

    added search result page

--- src/components/Pages/SearchResult.tsx
+++ src/components/Pages/SearchResult.tsx
@@ -1,13 +1,27 @@
+import { useLocation } from "react-router-dom";
 import Header from "../Header";
 import Posts from "../Posts";
 import CurrentUser from "../User/CurrentUser";
+import SearchInfo from "../Posts/SearchInfo";
+import { IPost } from "../../interfaces/Post/IPost";
 
 function SearchResult( ) {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    const query = searchParams.get('q');
+    
+    const posts : IPost[] = [
+        { id: 1, user : {id: 1, name: 'botcity', identifier: 'gpv', image: './bagre.jpeg'}, content: 'Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. ', publishedAt: (new Date()).toISOString()},
+        { id: 2, user : {id: 1, name: 'botcity', identifier: 'gpv', image: './bagre.jpeg'}, content: 'Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. ', publishedAt: (new Date()).toISOString()},
+        { id: 3, user : {id: 1, name: 'botcity', identifier: 'gpv', image: './bagre.jpeg'}, content: 'Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. Senhor. ', publishedAt: (new Date()).toISOString()}
+    ]
+
     const loggedUser = {id: 1, identifier: '@gpv', name: 'piva', image: null}
+
     return ( 
     <>
         <Header/>
-        <Posts posts={[]} header={undefined}/>
+        <Posts posts={posts} header={<SearchInfo query={query}/>}/>
         <CurrentUser user={loggedUser}/>
     </>
  );
---
+++ src/components/Posts/SearchInfo/index.tsx
@@ -0,0 +1,22 @@
+import styles from './SearchInfo.module.css'
+
+interface Props {
+    query : string | null;
+}
+
+function SearchInfo({query} : Props) {
+    return ( 
+        <div className={styles.searchInfo}>
+            <div className={styles.searchInfoQuery}>
+                <span>Exibindo resultados de:</span>
+                <span className={styles.textInfo}>{query}</span>            
+            </div>
+            <div className={styles.searchInfoDetails}>
+                <span>tags:</span>
+                <span>phrases:</span>
+            </div>
+        </div>
+    );
+}
+
+export default SearchInfo;
